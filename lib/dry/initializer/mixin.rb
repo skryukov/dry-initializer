1a2ef89d032428b11c435652e13665bf578451a3
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2021-11-02 22:16:09 +0300

    rubocop -a --only Style/CommentedKeyword

--- lib/dry/initializer/mixin.rb
+++ lib/dry/initializer/mixin.rb
@@ -6,7 +6,8 @@ module Dry
     module Mixin
       extend  DSL              # @deprecated
       include Dry::Initializer # @deprecated
-      def self.extended(klass) # @deprecated
+      # @deprecated
+      def self.extended(klass)
         warn "[DEPRECATED] Use Dry::Initializer instead of its alias" \
              " Dry::Initializer::Mixin. The later will be removed in v2.1.0"
         super
